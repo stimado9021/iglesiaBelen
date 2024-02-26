7970ea7d283ea607a991c6ed97706675ee37f7a7
stimado9021 <quinteroenrrique321@gmail.com>
2024-02-25 21:01:49 -0500

    first commit

---
+++ src/routes/personasRouter.js
@@ -0,0 +1,45 @@
+import express from "express"
+const router = express.Router();
+import {pool} from "../database.js"
+
+router.get('/blog',(req,res)=>{
+    res.render('blog')
+})
+
+router.get('/addPerson', async (req, res) => {
+    try {
+        const [status] = await pool.query('SELECT * FROM  statusrole')
+        console.log(status)
+         res.render('personas/addPerson',{status})
+    } catch (error) {
+        
+    }
+   
+})
+
+router.post('/addPerson',async(req,res)=>{
+try {
+    const {nombres,apellidos,cedula,correo,phone}=req.body;
+    const newPerson={
+        nombres,apellidos,cedula,correo,phone    
+    }
+    await pool.query("INSERT INTO usuarios SET ?",[newPerson])  
+    res.redirect('/listarPersona'); 
+} catch (error) {
+    res.status(500).json({message: error.message});
+}    
+})
+//04126681744 ipazulca
+router.get('/listarPersona', async (req, res) => {
+   
+    try {
+       
+        const [result] = await pool.query('SELECT * FROM usuarios')
+        console.log(result)
+        res.render('personas/listarPersonas', {personas: result})
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+})
+
+export default router
